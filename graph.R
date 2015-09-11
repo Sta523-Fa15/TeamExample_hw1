8c1bfa4875930e3c890acde2c54d089b92f653a4
Colin Rundel <rundel@gmail.com>
2015-09-11 15:46:03 -0400

    Added graph.R stub

---
+++ graph.R
@@ -0,0 +1,24 @@
+is_valid = function(g)
+{
+  TRUE
+}
+
+is_connected = function(g, v1, v2)
+{
+  TRUE
+}
+
+is_isomorphic = function(g1, g2)
+{
+  TRUE
+}
+
+is_undirected = function(g)
+{
+  TRUE
+}
+
+shortest_path = function(g, v1, v2)
+{
+  c()
+}
